490d45e622ccbf5d0f74a1310cbbd8063c3fae2a
hole <phanleho002@gmail.com>
2024-10-26 19:34:21 +0700

    feat: initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from '@/components/ui/button';
+
+export default function Home() {
+  return (
+    <div className="flex h-screen w-screen justify-center items-center">
+      <Button variant="outline">BASE PROJECT 1</Button>
+    </div>
+  );
+}
